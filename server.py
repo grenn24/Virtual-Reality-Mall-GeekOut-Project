639f24f08dcaab844f9ce02d2b82e21be8af26a1
Glitch (flask-demo) <none>
2021-06-24 14:24:46 +0000

    🐑🖼 Checkpoint ./server.py:33185219/1530 ./templates/index.html:33185219/810 ./templates/result.html:33185219/271 ./model.py:33185219/533

--- server.py
+++ server.py
@@ -8,7 +8,19 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return "Hello world!"
+  return render_template("index.html")
+
+@app.route("/result", methods=["GET", "POST"])
+def result():
+  if request.method == 'POST' and len(dict(request.form)) > 0:
+    userdata = dict(request.form)
+    print(userdata)
+    book = userdata["book"][0]
+    character = model.get_character(book)
+    gif_url = model.get_gif(character)
+    return render_template("result.html", character=character, gif_url=gif_url)
+  else:
+    return "Sorry, there was an error."
   
 if __name__ == "__main__":
   app.run()
